410b619dc11bd6f4e7099c6b36cc0e0d4901c38b
Aliakwe <aliakwe@yahoo.com>
2020-01-06 14:28:49 +0100

    update

--- src/redux/actions/artworkAction.js
+++ src/redux/actions/artworkAction.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import axios from "axios";
+import axios from 'axios';
 import {
   GET_ARTWORKS,
   GET_ARTWORK,
@@ -9,59 +9,59 @@ import {
   MY_ARTWORKS,
   CLEAR_ERROR,
   UI_LOADING
-} from "./types";
+} from './types';
 
-export const getArtworks = () => (dispatch) => {
+export const getArtworks = () => dispatch => {
   dispatch({ type: UI_LOADING });
   axios
-    .get("https://findartt.herokuapp.com/api/v1/art/find")
-    .then((response) => {
+    .get('https://findartt.herokuapp.com/api/v1/art/find')
+    .then(response => {
       dispatch({ type: GET_ARTWORKS, payload: response.data.data });
       console.log(response.data.data);
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(error);
     });
   dispatch({ type: CLEAR_ERROR });
 };
 
-export const getArtwork = (id) => (dispatch) => {
+export const getArtwork = id => dispatch => {
   dispatch({ type: UI_LOADING });
   axios
     .get(`https://findartt.herokuapp.com/api/v1/art/find/${id}/summary`)
-    .then((response) => {
+    .then(response => {
       dispatch({ type: GET_ARTWORK, payload: response.data.data });
       console.log(response.data.data);
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(error);
     });
   dispatch({ type: CLEAR_ERROR });
 };
 
-export const artworkBid = (bid) => (dispatch) => {
+export const artworkBid = bid => dispatch => {
   dispatch({ type: UI_LOADING });
   axios
-    .post("/art/bid", bid)
-    .then((response) => {
+    .post('https://findartt.herokuapp.com/api/v1/art/bid', bid)
+    .then(response => {
       dispatch({ type: BID_ARTWORK, payload: response.data.message });
-      console.log("bid", response.data.message);
+      console.log('bid', response.data.message);
     })
-    .catch((error) => {
+    .catch(error => {
       dispatch({ type: BID_MESSAGE, payload: error.response.data.message });
       console.log(error.response.data.message);
     });
   dispatch({ type: CLEAR_ERROR });
 };
 
-export const myArtworks = () => (dispatch) => {
+export const myArtworks = () => dispatch => {
   dispatch({ type: UI_LOADING });
   axios
-    .get("https://findartt.herokuapp.com/api/v1/art/owner/find")
-    .then((response) => {
+    .get('https://findartt.herokuapp.com/api/v1/art/owner/find')
+    .then(response => {
       dispatch({ type: MY_ARTWORKS, payload: response.data.data });
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(error);
     });
   dispatch({ type: CLEAR_ERROR });
